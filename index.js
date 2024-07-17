517bef72517ccd3d05e40ff5c517c11b6f35b6bb
Dev Ashrit Behera <devashritcode@gmail.com>
2024-07-17 21:05:47 +0530

    .env file changed

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express')
+const dotenv = require("dotenv").config()
 const cors = require('cors')
 
 const app = express()
+const port = process.env.PORT
 
 app.use(cors())
 app.use('/',(req,res) => {
-    res.send("Hello");
+    res.status(200).send("The server is running");
 })
 
-app.listen(8000,()=>{
-    console.log(`The server running at port ${8000}`)
+app.listen(port,()=>{
+    console.log(`The server running at port ${port}`)
 })
